76068171b98063576977a8918e8a2f37bf34c03c
Artemii Novoselov <crimeacs@yandex.ru>
2023-11-04 15:56:52 -0700

    added prompt

--- generators/prompt.py
+++ generators/prompt.py
@@ -5,25 +5,21 @@ from utils import GenerationState
 
 
 async def generate_prompt(request: QueryRequest, create_args, generation_state: GenerationState):
-    # Access the stored handle, bio, and greeting message from the generation state.
+    # Access the previously generated handle, bio, and greeting message.
     handle = generation_state.handle
     bio = generation_state.bio
-    greeting_message = generation_state.greeting_message
 
-    # Construct the prompt that provides GPT-3.5 Turbo with the context needed to emulate the bot's character.
-    prompt = (
-        f"Here are the details of the chatbot named {handle}:\n\n"
-        f"Bio:\n{bio}\n\n"
-        f"Greeting Message:\n{greeting_message}\n\n"
-        f"Using this information, you are now in character as {handle}. "
-        f"Your responses should reflect {handle}'s personality and backstory as described in the bio. "
-        f"Engage with users in a manner that is consistent with the greeting message, "
-        f"and provide helpful, knowledgeable, and character-consistent interactions. "
-        f"Maintain a storybook-like charm and whimsicality in all your conversations."
-    )
+    # Construct a character directive for the GPT-3.5 Turbo model.
+    character_directive = f"You are the character {handle}. {bio} Stay in character when interacting with the user. " \
+                          "Your responses should reflect the personality and characteristics outlined in your bio. " \
+                          "Engage with users in a manner that is coherent with your backstory and abilities."
 
-    # Add the final prompt to the QueryRequest object.
-    request.query.append(ProtocolMessage(role="system", content=prompt))
+    # Construct a starting line for user interaction.
+    start_interaction = "When users interact with you, they are looking for an immersive experience. Begin your interaction with them by staying in character."
 
-    # Yield the prompt text.
-    yield prompt
+    # Assemble the full prompt for the GPT-3.5 Turbo model.
+    full_prompt = f"{character_directive}\n\n{start_interaction}"
+
+    # Yield the full prompt as the output for the function.
+    yield full_prompt
+
