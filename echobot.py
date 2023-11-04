d872d00c2c29ad1ce448f08498c81a521826a64e
Artemii Novoselov <crimeacs@yandex.ru>
2023-11-04 13:19:33 -0700

    added template

---
+++ echobot.py
@@ -0,0 +1,21 @@
+"""
+
+Sample bot that echoes back messages.
+
+This is the simplest possible bot and a great place to start if you want to build your own bot.
+
+"""
+from __future__ import annotations
+
+from typing import AsyncIterable
+
+from fastapi_poe import PoeBot
+from fastapi_poe.types import PartialResponse, QueryRequest
+
+
+class EchoBot(PoeBot):
+    async def get_response(
+        self, request: QueryRequest
+    ) -> AsyncIterable[PartialResponse]:
+        last_message = request.query[-1].content
+        yield PartialResponse(text=last_message)
